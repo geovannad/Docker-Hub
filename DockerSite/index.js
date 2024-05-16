1ef73e69ce91346ea3d1df23e0a9721cfbf628dd
IRenatoBarros <renatofilho9722@gmail.com>
2024-05-16 13:02:48 -0300

    pagina atividade 1.1

---
+++ DockerSite/index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var toggleButton = document.getElementById('toggle');
+    var menu = document.getElementById('menu');
+  
+    toggleButton.addEventListener('click', function() {
+      menu.classList.toggle('open');
+    });
+  });
+  
---
+++ DockerSite/matheusCodigo/principal/index.js
@@ -0,0 +1,8 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     var toggleButton = document.getElementById('toggle');
+//     var menu = document.getElementById('menu');
+  
+//     toggleButton.addEventListener('click', function() {
+//       menu.classList.toggle('open');
+//     });
+//   });
