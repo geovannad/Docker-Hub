4b57b698fe900e5f96c8fc3cecfd6173193078f1
geovannad <geovanna.diniz@germinare.org.br>
2024-05-21 10:51:32 -0300

    adicionando ondas

--- DockerSite/matheusCodigo/principal/index.js
+++
@@ -1,8 +0,0 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     var toggleButton = document.getElementById('toggle');
-//     var menu = document.getElementById('menu');
-  
-//     toggleButton.addEventListener('click', function() {
-//       menu.classList.toggle('open');
-//     });
-//   });
